0baebb51d31092b2facc7bfccc055e322ac9cd48
xLeDocteurx <lenoirc.fr@gmail.com>
2018-06-18 14:19:49 +0200

    Raw integration of firebase for connecting users

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,319 +1,75 @@
 let socket;
-// var JSON_samplePack_Samples = [];
-// var JSON_tracks = [];
 
-var fr = 50;
-var makeymakeyMode = false;
+socket = io.connect();
 
-var isPlaying = false;
+firebase.auth().onAuthStateChanged(function(user) {
+  let logged = document.getElementById("logged-button");
+  let loggin = document.getElementById("loggin-button");
+  let newuser = document.getElementById("newuser-button");
+  if (user) {
+    loggin.style.display = "none";
+    newuser.style.display = "none";
+    logged.style.display = "initial";
 
-var bpm = 105;
-var step_Type = 4;
-//var bar_type = 4;
+    document.getElementById("userpage").innerText = user.email;
+  } else {
+    loggin.style.display = "initial";
+    newuser.style.display = "initial";
+    logged.style.display = "none";
+  }
+});
 
-var bar = 60 / bpm * 1000;
-var stepsResolution = 32;
+function login() {
+  let email = document.getElementById("inputEmail").value;
+  let password = document.getElementById("inputPassword").value;
 
-var step = bar / stepsResolution;
-
-///////////////////////////////////////////////
-var currentStep = 0;
-var update_timeout;
-//////////////////////////////////////////////
-
-
-
-// var all_samplePacks = [];
-// var all_songs = [];
-// var all_tracks = [];
-
-function preload() {
-  socket = io.connect();
-  ////////////////////// CONVERT AND IMPORT JSON AS SAVE FILES /////////////////////////
-    // getJSON();
-    //////////////////////////////////////////////
-
-
-    // song = document.createElement("audio");
-    // song.scr = "../songs/skull-snaps-its-a-new-day-1973.mp3";
-
-}
-
-function setup() {
-
-    // frameRate(fr);
-
-    // all_tracks.forEach(track => {
-    //     loadTrack(track);
-    //     track.clips.forEach(clip => {
-    //         loadDrumRack_clip(track, clip);
-    //     });
-    //     track.channels.forEach( channel => {
-    //         console.log('channel : ');
-    //         console.log(channel);
-    //         // channel.sample.setVolume(1.0);
-    //         // channel.sample.playMode(channel.sample.playMode);
-    //     });
-    // });
-
-    // for (var channel of all_tracks[0].channels) {
-    //     channel.sample.setVolume(1.0);
-    //     channel.sample.playMode(JSON_samplePack_Samples.samples[channel.index].playmode);
-    // }
-
-    //////////////////////////////////////////
-}
-
-function draw() {
-
-    //  A netoyer ?? // ( simplifier le code d'incrémentation )
-    bar = 60 / bpm * 1000 * 4;
-    step = bar / stepsResolution;
-}
-
-function update() {
-    //////////////////////////////////////////////
-    //  setTimeout("update()", step);
-    update_timeout = setTimeout(() => {
-        update(step);
-    }, step);
-
-    /////////////////////////////////////////////
-
-    launchSteps(currentStep);
-
-    /////////////////////////////////////////////
-
-    currentStep += 1;
-    console.log(currentStep);
-    if (currentStep >= stepsResolution) {
-        currentStep = 0;
-    }
-}
-
-function changemakeymakeyMode() {
-
-    if (makeymakeyMode == true) {
-        makeymakeyMode = false;
-        console.log("makeymakeyMode : " + makeymakeyMode);
-    } else {
-        makeymakeyMode = true;
-        console.log("makeymakeyMode : " + makeymakeyMode);
-    }
-}
-
-function sessionPlay() {
-
-    sessionStop();
-
-    isPlaying = true;
-    update();
-    //    start = Date.now();
-}
-
-function sessionStop() {
-
-    isPlaying = false;
-    currentStep = 0;
-    clearTimeout(update_timeout);
-    //    start = null;
-}
-
-function sessionPause() {
-
-    isPlaying = false;
-    clearTimeout(update_timeout);
-    //    start = null;
-}
-
-function setBPMx(tValue) {
-    bpm = tValue;
-    console.log("Tempo changed to : " + bpm);
-}
-
-
-function previousScene() {
-
-    for (var track of all_tracks) {
-
-        track.currentClip -= 1;
-
-        console.log(track.currentClip);
-    }
-}
-
-function addScene () {
-
-}
-
-function nextScene() {
-
-    for (var track of all_tracks) {
-
-        track.currentClip += 1;
-
-        console.log(track.currentClip);
-    }
-}
-
-function launchSteps(currentStep) {
-
-    all_tracks.forEach((track, track_index) => {
-        let clip = track.clips[track.currentClip];
-        let step = clip.steps[currentStep];
-        let samplePack = all_samplePacks[track.samplePack];
-        step.forEach((note, note_index) => {
-
-
-
-            for (let k = 0; k < step.length; k++) {
-                console.log(step[k]);
-                console.log(j);
-                if (step[k] == j) {
-                    track.channels[j - 1].channel.sample.play();
-                }
-            }
-        });
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then(function() {
+      console.log("authentification is a success");
+    })
+    .catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ...
+      alert(error.message);
     });
-    for (let i = 0; i < all_tracks.length; i++) {
-        for (let j = 0; j <= track.channels.length; j++) {
-
-        }
-    }
 }
 
-function showTrack() {
-    
+function logout() {
+  firebase
+    .auth()
+    .signOut()
+    .catch(function(error) {
+      console.log("signing out is a FAILURE !");
+    });
+
+  // socket.emit('subuser');
+  // console.log("signing out is a success !");
+  // localStorage.removeItem('user_email');
 }
 
-function clip_Edit(track_ID, clip_ID) {
+function register() {
+  let email = document.getElementById("newuserEmail").value;
+  let password = document.getElementById("newuserPassword").value;
+  let username = document.getElementById("newUsername").value;
 
-    console.log("track, " + all_tracks[track_ID] + " nbr of channels : " + all_tracks[track_ID].channels.length);
-    document.getElementById("clip-editor").innerHTML = ` <div id="pads-container" class="d-inline p-0">
-                                                                </div>
-                                                                <div id="channels-container" class="d-inline p-0">
-                                                                    <div id="channels-guide">
-                                                                    </div>
-                                                                </div>`;
-    for (var channel of all_tracks[track_ID].channels) {
-        loadDrumRack_Channel(channel, track_ID, clip_ID);
-    }
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ...
+      console.log("error : " + error.message);
+    });
+
+  // let user = {
+  //     email : email,
+  //     username : username
+  // };
+  // socket.emit('register', user);
 }
-
-function editStep(thiss, step, trackID, channelID, clipID) {
-    ///////////////////////////////////
-
-    var track = all_tracks[trackID];
-    var clip = track.clips[clipID];
-    var steps = clip.steps;
-
-    var index = clip.steps.indexOf(step);
-    // var index = step;
-
-    // console.log("track : " + track);
-    // console.log("clip : " + clip);
-    console.log("steps : " + steps);
-    // console.table("channel : " + channelID);
-    // console.log("step : " + index);
-
-    if (thiss.checked == false) {
-        var supr = clip.steps[step].splice(index, 1);
-        console.log("element supprimé : " + supr);
-        console.log(supr);
-    } else {
-        console.log(step);
-        clip.steps[step].splice(0, 0, track.channels[channelID].id + 1);
-    }
-
-}
-
-function clip_Launch(track_ID, clip_ID) {
-    console.log(track_ID);
-    console.log(clip_ID);
-    console.log(all_tracks);
-    console.log(all_tracks[track_ID]);
-    all_tracks[track_ID].currentClip = clip_ID;
-
-    if (!isPlaying)
-        sessionPlay();
-}
-
-function loadTrack(track) {
-    //pour le "create new track"
-    // var track = new Track(type);
-
-    var tracksContainer = document.getElementById("tracks-container");
-    var ttemplate = new track_Template(track);
-    tracksContainer.innerHTML += ttemplate.html;
-}
-
-function loadDrumRack_clip(track, clip) {
-
-    var clipsContainer = document.getElementById(track.name);
-    var cliptemplate = new drumRackClip_Template(track, clip);
-    clipsContainer.innerHTML += cliptemplate.html;
-
-    // var stepsContainer = document.getElementById("channels-container");
-    // var stepstemplate = new drumRackChannel_Template(clip);
-    // stepsContainer.innerHTML += stepstemplate.html;
-}
-
-
-/////// Y INTEGRER LE FOR OF DE LA BOUCLE DE CHARGEMENT DU SETUP
-function loadDrumRack_Channel(channel, trackID, clipID) {
-
-    var track = all_tracks[trackID];
-    var clip = track.clips[clipID];
-    // var channel = track.channels[j - 1];
-
-    if (channel != null) {
-        var channelsGuide = document.getElementById("channels-guide");
-        console.log("loaddrumRack_Channel : " + channel.sample);
-        var drtemplate = new drumRackChannel_Template(channel, clipID);
-
-        insertAfter(channelsGuide, drtemplate.element);
-        ////////// RAJOUT DES STEPS DE LA PISTE //////////
-        for (var step of clip.steps) {
-            var drclips = new stepTemplate(track, channel, clip, step);
-            //            drtemplate.appendChild(drclips);
-            document.getElementById(`${channel.name + "_currentClip"}`).appendChild(drclips.element);
-        }
-
-    }
-}
-
-//////////////////////////////////
-///////////////////////////
-function insertAfter(referenceNode, newNode) {
-    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
-}
-//////////////////////////////////
-///////////////////////////
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* Pour plus tard, un système d'upload de samples */
-/* ( Pour l'instant nous utiliserons exclusivement
-une banque de samples prédéfinis par soucis de simplicité ) */
-/*function loadSound(sample, channel) {
-
-    var reader = new FileReader();
-    reader.onload = function (e) {
-      $('#sound_file')
-        .attr('src', e.target.result);
-        sound = e.target.result;
-    };
-    reader.readAsDataURL(sample.files[0]);
-} */
--- utils/utils.js
+++ utils/utils.js
@@ -8,35 +8,3 @@ exports.commit = function (data) {
     console.log('JSON successfully modified!');
   });
 };
-
-exports.newProject = (data) => {
-  
-}
-
-exports.saveProject = (data) => {
-  
-}
-
-exports.loadProject = (data) => {
-  
-}
-
-exports.add_User = (data) => {
-
-  var i = all_Clients.indexOf(all_Clients.find(element => {
-    return element.sockid == socket.id;
-  }));
-  all_Clients[i].email = data;
-
-  io.sockets.emit('refresh', all_Clients);
-}
-
-exports.sub_User = (data) => {
-
-
-  console.log("substracted datas from all_Clients :");
-  var i = all_Clients.indexOf(data.id);
-  all_Clients.splice(i, 1);
-
-  io.sockets.emit('refresh', all_Clients);
-};
