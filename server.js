c63aaae88c51fb3f7f84398aefab19f71cb87e1a
Simon Darmandieu <simon.darmandieu@gmail.com>
2018-06-22 11:08:13 +0200

    some views and create post in server

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 let bodyparser = require('body-parser');
 let express = require('express');
-let socket = require('socket.io');
-let moment = require('moment');
+//let socket = require('socket.io');
+//let moment = require('moment');
 let fs = require('fs');
+let MongoClient = require('mongodb').MongoClient
 
 const webport = 8080;
 
@@ -11,20 +12,45 @@ app.set('view engine', 'ejs');
 app.use(express.static('public')); // fait appel au dossier Public, avec Css, Frameworks, Img et script.js
 app.use(bodyparser.urlencoded({ extended: false}));
 
+var db
+
+MongoClient.connect('mongodb://vaness:azerty00@ds161520.mlab.com:61520/blog_v3', function (err, client)  {
+  if (err) return console.log(err)
+  db = client.db('blog_v3')
+
 let server = app.listen(process.env.PORT || webport);
-let io = socket(server);
+//let io = socket(server);
+
+})
+
+
 
 
 // index.ejs = Accueil
-app.get('/', (req, res) => {
+app.get('/', function (req, res) {
    // let url = 'mongodb://vaness:azerty00@ds161520.mlab.com:61520/blog_v3';
     res.render('index');
 });
 
-io.on('connection', (socket) => {
+app.get('/create' , function(req,res)  {
+	res.render('create');
+})
 
-    // console.log(`//////////////////////////////////////////////////////////////////////////////////////////`);
-    // console.log(`Un utilisateur non enregistré s'est connecté au serveur : ${socket.id}`);
-    // console.log(`//////////////////////////////////////////////////////////////////////////////////////////`);
+app.post('/create/post' , function(req,res){
+	db.collection('posts').save(req.body, function (err, result) {
+    if (err) return console.log(err)
+    console.log('saved to database')
+    res.render('index')
+  })
+})
 
-});
+
+
+
+// io.on('connection', (socket) => {
+
+//     // console.log(`//////////////////////////////////////////////////////////////////////////////////////////`);
+//     // console.log(`Un utilisateur non enregistré s'est connecté au serveur : ${socket.id}`);
+//     // console.log(`//////////////////////////////////////////////////////////////////////////////////////////`);
+
+// });
