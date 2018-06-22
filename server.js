82edbb37025bdeed2aa8cf62febf6c78cca0e3f8
Simon Darmandieu <simon.darmandieu@gmail.com>
2018-06-22 12:01:08 +0200

    removing few comments

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 let bodyparser = require('body-parser');
 let express = require('express');
-//let socket = require('socket.io');
 //let moment = require('moment');
 let fs = require('fs');
 let MongoClient = require('mongodb').MongoClient
@@ -19,38 +18,27 @@ MongoClient.connect('mongodb://vaness:azerty00@ds161520.mlab.com:61520/blog_v3',
   db = client.db('blog_v3')
 
 let server = app.listen(process.env.PORT || webport);
-//let io = socket(server);
 
 })
 
-
-
-
 // index.ejs = Accueil
 app.get('/', function (req, res) {
-   // let url = 'mongodb://vaness:azerty00@ds161520.mlab.com:61520/blog_v3';
-    res.render('index');
+    db.collection('posts').find().toArray( function (err, result) {
+    if (err) return console.log(err)
+    res.render('index.ejs', {posts: result})
+  })
 });
 
+// afficher la page de création
 app.get('/create' , function(req,res)  {
 	res.render('create');
 })
 
+// save le post dans la DB , et rediriger vers l'accueil
 app.post('/create/post' , function(req,res){
 	db.collection('posts').save(req.body, function (err, result) {
     if (err) return console.log(err)
     console.log('saved to database')
-    res.render('index')
+    res.redirect('/')
   })
 })
-
-
-
-
-// io.on('connection', (socket) => {
-
-//     // console.log(`//////////////////////////////////////////////////////////////////////////////////////////`);
-//     // console.log(`Un utilisateur non enregistré s'est connecté au serveur : ${socket.id}`);
-//     // console.log(`//////////////////////////////////////////////////////////////////////////////////////////`);
-
-// });
